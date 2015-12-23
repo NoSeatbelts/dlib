39497629369fad5e729c834cee2a6adce21bae9f
dnephi <daniel.baker@aruplab.com>
2015-12-23 15:49:39 -0700

    Change logging utilities.

--- flow_util.h
+++ flow_util.h
@@ -5,7 +5,12 @@
 #include "stdio.h"
 
 void log_abort(char *str) {
+	fprintf(stderr, "[E:%s] ", __func__);
 	fprintf(stderr, str);
 	exit(EXIT_FAILURE);
 }
+
+#ifndef ABORT
+#define ABORT(message) do {fprintf(stderr, "[E:%s] ", __func__); fprintf(stderr, message); exit(EXIT_FAILURE);} while(0)
+#endif
 #endif
