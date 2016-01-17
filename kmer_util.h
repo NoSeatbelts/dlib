0253a373a3432d01b99a37b8aeab2b3384cdcff1
dnephi <daniel.baker@aruplab.com>
2016-01-16 23:04:47 -0700

    added kmer2str

--- kmer_util.h
+++ kmer_util.h
@@ -3,4 +3,19 @@
 #define MAX_KMER 31
 #define DEFAULT_KMER 21
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	inline void kmer2cstr(uint64_t kmer, int k, char *buf)
+	{
+		buf += k;
+		while(k) *--buf = ((kmer >> (2 * --k)) & 0x3);
+		LOG_DEBUG("kmer %lu has now become string '%s'.\n", kmer, buf);
+	}
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
