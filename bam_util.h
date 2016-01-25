3c39e608912b9e8aba6c3a77ec2561bc5f550e13
dnephi <daniel.baker@aruplab.com>
2016-01-24 20:03:37 -0700

    Refactoring to handle a multiple definitions problem.

--- bam_util.h
+++ bam_util.h
@@ -17,10 +17,7 @@ typedef void (*single_fn)(bam1_t *b);
 typedef void (*single_aux)(bam1_t *b, void *data);
 typedef int (*single_aux_check)(bam1_t *b, void *data);
 
-#ifndef SEQ_COMP_TABLE
-#define SEQ_COMP_TABLE
-const int8_t seq_comp_table[16] = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-#endif
+const int8_t seq_comp_table[16];
 
 #ifdef __cplusplus
 extern "C" {
