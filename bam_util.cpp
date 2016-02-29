888daeaa32bb047b13cda88f9be87cde5add1441
dnephi <daniel.baker@aruplab.com>
2016-02-29 00:34:16 -0700

    Add an assert and make sure we're not using the unallocated parts of the hashmap.

--- bam_util.cpp
+++ bam_util.cpp
@@ -168,15 +168,17 @@ namespace dlib {
         plp_aux<T> aux = plp_aux<T>(auxen, this, minMQ);
         plp = bam_plp_init(&read_bam<plp_aux<T>>, (void *)&aux);
         for(khiter_t ki = kh_begin(bed); ki != kh_end(bed); ++ki) {
+            if(!kh_exist(bed, ki)) continue;
             for(unsigned i = 0; i < kh_val(bed, ki).n; ++i) {
                 const int start = get_start(kh_val(bed, ki).intervals[i]);
                 const int stop = get_stop(kh_val(bed, ki).intervals[i]);
                 const int bed_tid = (int)kh_key(bed, ki);
                 int tid, n_plp, pos;
                 if(iter) hts_itr_destroy(iter);
-                iter = bam_itr_queryi(idx,bed_tid, start, stop);
+                iter = bam_itr_queryi(idx, bed_tid, start, stop);
                 bam_plp_reset(plp);
                 while(bam_plp_auto(plp, &tid, &pos, &n_plp) != 0) {
+                    assert(tid == (unsigned)bed_tid);
                     if(pos < start) continue;
                     if(pos >= stop) break;
                     fn(pileups, n_plp, aux->plp_aux);
