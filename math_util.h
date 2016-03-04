cb9eec74d3e418e660f624774ea0ee86fb612df5
dnephi <daniel.baker@aruplab.com>
2016-03-04 10:30:40 -0700

    Cleanup.

--- math_util.h
+++ math_util.h
@@ -6,17 +6,17 @@
 #include <assert.h>
 
 namespace dlib {
-	double hellinger(double* arr1, double* arr2, size_t length);
-	static inline int64_t ipow(int base, int exp)
-	{
-	    int64_t ret = 1;
-	    while(exp) {
-	        if(exp & 1) ret *= base;
-	        exp >>= 1;
-	        base *= base;
-	    }
-	    return ret;
-	}
+    double hellinger(double* arr1, double* arr2, size_t length);
+    static inline int64_t ipow(int base, int exp)
+    {
+        int64_t ret = 1;
+        while(exp) {
+            if(exp & 1) ret *= base;
+            exp >>= 1;
+            base *= base;
+        }
+        return ret;
+    }
 }
 
 
