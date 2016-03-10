a5c81d50201d24c4ad0b593039ed927c15114cb9
dnephi <daniel.baker@aruplab.com>
2016-03-10 10:04:47 -0700

    Added bitset_qcfail.

--- cstr_util.h
+++ cstr_util.h
@@ -27,6 +27,9 @@ KSEQ_INIT(gzFile, gzread)
 #endif
 
 #ifdef __cplusplus
+#include <string>
+
+using namespace std::string_literals; // For ""s suffix for std::string literals.
 namespace dlib {
 
 /* sprintf's to the buffer in string.
