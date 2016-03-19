27d7dbad4ebb1ebb9e2c7c8a90b043ba392eb921
Daniel Baker <daniel.baker@aruplab.com>
2016-03-18 22:34:15 -0600

    Style

--- misc_util.cpp
+++ misc_util.cpp
@@ -5,9 +5,8 @@ namespace dlib {
     void string_fmt_time(std::string& ret) {
         time_t now = time(NULL);
         struct tm *t = localtime(&now);
-        while(strftime((char *)ret.data(), ret.capacity(), "%Y:%m:%d %H:%M", t) == 0)
+        while(!strftime((char *)ret.data(), ret.capacity(), "%Y:%m:%d %H:%M", t))
             ret.reserve(ret.size() << 1);
-        return;
     }
 
 }
