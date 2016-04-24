fc186bb6e0f8527246c3c3879a78ef833ceb45b3
dnephi <daniel.baker@aruplab.com>
2016-04-23 20:51:42 -0600

    Coerce to 64-bit integer BEFORE bitshifting.

--- sort_util.h
+++ sort_util.h
@@ -14,19 +14,19 @@
 #ifdef __cplusplus
 #    define ucs_sort_mate_key(a) ((uint64_t)(a->core.mtid) << 32 | (bam_itag(a, "MU") + 1) << 1 | bam_is_mrev(a))
 #    define ucs_sort_core_key(a) (((uint64_t)(a->core.tid) << 32) | ((dlib::get_unclipped_start(a)+1) << 2) | (bam_is_rev(a) << 1) |bam_is_r1(a))
-#    define ucs_se_sort_key(a) ((uint64_t)(a->core.tid)<<32|((dlib::get_unclipped_start(a)+1)<<2)|bam_is_rev(a))
+#    define ucs_se_sort_key(a) ((uint64_t)(a->core.tid) <<32|((dlib::get_unclipped_start(a)+1)<<2)|bam_is_rev(a))
 #else
 #    define ucs_sort_mate_key(a) ((uint64_t)(a->core.mtid) << 32 | (bam_itag(a, "MU") + 1) << 1 | bam_is_mrev(a))
 #    define ucs_sort_core_key(a) (((uint64_t)(a->core.tid) << 32) | ((get_unclipped_start(a)+1) << 2) | (bam_is_rev(a) << 1) |bam_is_r1(a))
-#    define ucs_se_sort_key(a) ((uint64_t)(a->core.tid)<<32|((get_unclipped_start(a)+1)<<2)|bam_is_rev(a))
+#    define ucs_se_sort_key(a) ((uint64_t)(a->core.tid) <<32|((get_unclipped_start(a)+1)<<2)|bam_is_rev(a))
 #endif
 
 
 // Pos sort key macros
-#define bmfsort_core_key(a) ((uint64_t)a->core.tid<<32|(a->core.pos+1)<<2|bam_is_rev(a)<<1|bam_is_r1(a))
-#define bmfsort_mate_key(a) ((uint64_t)a->core.mtid<<32|(a->core.mpos+1)<<1|bam_is_mrev(a))
+#define bmfsort_core_key(a) ((uint64_t)(a->core.tid)<<32|(a->core.pos+1)<<2|bam_is_rev(a)<<1|bam_is_r1(a))
+#define bmfsort_mate_key(a) ((uint64_t)(a->core.mtid)<<32|(a->core.mpos+1)<<1|bam_is_mrev(a))
 
-#define bmfsort_se_key(a) ((uint64_t)a->core.tid<<32|((a->core.pos+1)<<2)|bam_is_rev(a))
+#define bmfsort_se_key(a) ((uint64_t)(a->core.tid)<<32|((a->core.pos+1)<<2)|bam_is_rev(a))
 
 // Other sort key macros
 #define sort_rlen_key(a) (((uint64_t)(b->core.l_qseq) << 10)|bam_itag(a, "LM"))
